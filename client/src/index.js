09bac8f53baa323508ebe8a491fdff98ca510f83
Amanda Nelson <amanda.gaylord@gmail.com>
2021-01-10 16:28:39 -0500

    src/index.js complete.

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import reducer from "./reducers/index";
 import "./index.css";
 import App from "./App";
+
+const store = createStore(reducer, applyMiddleware(thunk));
 
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
-    <App />, 
-    rootElement
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
 );
 
 //Task List:
-//1. Add in all necessary components and libary methods.
-//2. Create a store that includes thunk middleware support.
-//3. Wrap the App component in a react-redux Provider element.
+//1. ✅Add in all necessary components and library methods.
+//2. ✅Create a store that includes thunk middleware support.
+//3. ✅Wrap the App component in a react-redux Provider element.
